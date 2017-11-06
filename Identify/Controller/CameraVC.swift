275b1546f86d76b644a0ab7dc54040e0ba15f279
Dan Lindsay <dlindsay72@hotmail.com>
2017-11-06 13:32:25 -0500

    basic project setup done

--- Identify/Controller/CameraVC.swift
+++ Identify/Controller/CameraVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CameraVC.swift
 //  Identify
 //
 //  Created by Dan Lindsay on 2017-11-06.
@@ -8,18 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CameraVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
